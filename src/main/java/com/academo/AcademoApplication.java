7ab8a3588c81b24f06a4d1e4f6ea7e75be5932a5
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2025-08-19 17:39:52 -0300

    First commit

---
+++ src/main/java/com/academo/AcademoApplication.java
@@ -0,0 +1,13 @@
+package com.academo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academo/AcademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
