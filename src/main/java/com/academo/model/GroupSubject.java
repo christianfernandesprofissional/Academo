ff05fe319b1529a9c2b4675903f103b182708a6c
Alberto Oliveira <alberto.bruno.s.oliveira.2004@gmail.com>
2025-08-23 07:47:48 -0300

    Fix: Correção de anotações e nome de colunas

--- src/main/java/com/academo/model/GroupSubject.java
+++ src/main/java/com/academo/model/GroupSubject.java
@@ -1,14 +1,20 @@
 package com.academo.model;
 
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_groups_subjects")
 public class GroupSubject {
     @EmbeddedId
     private GroupSubjectPK groupSubjectPK = new GroupSubjectPK();
+    @ManyToOne
+    @JoinColumn(name = "id_group")
+    @MapsId("idGroup")
+    private Group group;
+    @ManyToOne
+    @JoinColumn(name = "id_subject")
+    @MapsId("idSubject")
+    private Subject subject;
 
     public GroupSubjectPK getGroupSubjectPK() {
         return groupSubjectPK;
