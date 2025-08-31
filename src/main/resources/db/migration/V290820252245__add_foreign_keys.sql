e40310af51b94a9dd63bbc5edbb24fb4ce2e0091
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2025-08-31 14:44:44 -0300

    fix: correção de nomenclatura e query da migration V290820252245

--- src/main/resources/db/migration/V290820252245__add_foreign_keys.sql
+++ src/main/resources/db/migration/V290820252245__add_foreign_keys.sql
@@ -2,9 +2,9 @@
 ALTER TABLE "tb_profiles" ADD FOREIGN KEY ("id") REFERENCES "tb_users" ("id");
 
 ALTER TABLE "tb_groups_subjects" ADD FOREIGN KEY ("id_group") REFERENCES "tb_groups" ("id");
-ALTER TABLE "tb_groups_subjects" ADD FOREIGN KEY ("id_subject") REFERENCES "Tb_Subjects" ("id");
+ALTER TABLE "tb_groups_subjects" ADD FOREIGN KEY ("id_subject") REFERENCES "tb_subjects" ("id");
 
-ALTER TABLE "tb_activities" ADD FOREIGN KEY ("subject_id") REFERENCES "Tb_Subjects" ("id");
+ALTER TABLE "tb_activities" ADD FOREIGN KEY ("subject_id") REFERENCES "tb_subjects" ("id");
 
 ALTER TABLE "tb_groups" ADD FOREIGN KEY ("user_id") REFERENCES "tb_users" ("id");
 
